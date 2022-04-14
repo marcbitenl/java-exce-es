b9029ece842eddb926cea1c24f1b4d2b61ddaf51
marcb <marcbitenl@gmail.com>
2022-04-13 21:04:01 -0300

    Try e catch

--- src/Fluxo.java
+++ src/Fluxo.java
@@ -1,3 +1,5 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Fluxo {
 
 	public static void main(String[] args) {
@@ -8,7 +10,11 @@ public class Fluxo {
 
 	private static void metodo1() {
 		System.out.println("Ini do metodo1");
-		metodo2();
+		try {
+			metodo2();
+		} catch (ArithmeticException ex) {
+			System.out.println("ArithmeticException");
+		}
 		System.out.println("Fim do metodo1");
 	}
 
@@ -16,8 +22,11 @@ public class Fluxo {
 		System.out.println("Ini do metodo2");
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
-			int a = i/ 0;
+
+			int a = i / 0;
+
 		}
 		System.out.println("Fim do metodo2");
+
 	}
 }
